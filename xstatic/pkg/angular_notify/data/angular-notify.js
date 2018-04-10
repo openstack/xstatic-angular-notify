59bdf1db3a2b301ade80701e5d5baccf36cbb209
Xinni Ge <xinni.ge1990@gmail.com>
2018-04-10 18:45:06 +0900

    Update files to upstream
    1. update js file content.
    2. remove scss file and add css file.
    3. add the html template file.
    
    Change-Id: I328b902b43def31d0f1e8a31da8c2c427c2bfa65

--- xstatic/pkg/angular_notify/data/angular-notify.js
+++ xstatic/pkg/angular_notify/data/angular-notify.js
@@ -154,31 +154,3 @@ angular.module('cgNotify', []).factory('notify',['$timeout','$http','$compile','
         return notify;
     }
 ]);
-
-angular.module('cgNotify').run(['$templateCache', function($templateCache) {
-  'use strict';
-
-  $templateCache.put('angular-notify.html',
-    "<div class=\"cg-notify-message\" ng-class=\"[$classes, \n" +
-    "    $position === 'center' ? 'cg-notify-message-center' : '',\n" +
-    "    $position === 'left' ? 'cg-notify-message-left' : '',\n" +
-    "    $position === 'right' ? 'cg-notify-message-right' : '']\"\n" +
-    "    ng-style=\"{'margin-left': $centerMargin}\">\n" +
-    "\n" +
-    "    <div ng-show=\"!$messageTemplate\">\n" +
-    "        {$ $message $}\n" +
-    "    </div>\n" +
-    "\n" +
-    "    <div ng-show=\"$messageTemplate\" class=\"cg-notify-message-template\">\n" +
-    "        \n" +
-    "    </div>\n" +
-    "\n" +
-    "    <button type=\"button\" class=\"cg-notify-close\" ng-click=\"$close()\">\n" +
-    "        <span aria-hidden=\"true\">&times;</span>\n" +
-    "        <span class=\"cg-notify-sr-only\">Close</span>\n" +
-    "    </button>\n" +
-    "\n" +
-    "</div>"
-  );
-
-}]);
